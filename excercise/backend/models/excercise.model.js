1a2f9a70d8fd00e9b3d3df946d60767aa2aa13ee
Bert Keller <bertwinkeller@gmail.com>
2020-10-19 13:57:11 -0700

    new schema

--- excercise/backend/models/excercise.model.js
+++ excercise/backend/models/excercise.model.js
@@ -2,3 +2,16 @@ const mongoose = require('mongoose')
 
 const {Schema } = require('mongoose')
 
+const excerciseSchema = new Schema({
+    username: { type: String, required: true},
+    description: { type: String, required: true},
+    duration: { type: Number, required: true},
+    date: { type: Date, required: true}
+
+}, {
+    timestamps: true
+})
+
+const Exercise = mongoose.model('Exercise', exerciseSchema)
+
+module.exports = Exercise
