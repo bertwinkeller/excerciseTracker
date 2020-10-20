b95e9e2945bbafa9ff893f1e977d9f06f7cd0eb8
Bert Keller <bertwinkeller@gmail.com>
2020-10-19 19:13:42 -0700

    new routes

--- excercise/backend/server.js
+++ excercise/backend/server.js
@@ -23,6 +23,7 @@ const usersRouter = require('./routes/users')
 
 app.use('/exercises', exercisesRouter)
 app.use('/users', usersRouter)
+
 app.listen(port, () => {
     console.log(`Sever is running on port ${port}`)
 })
